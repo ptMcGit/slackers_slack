78bab87459acb0074cd6ae4b2d0b401080132d86
Zach H <zdhadgraft@gtcc.edu>
2016-07-01 16:55:23 -0400

    WIP modifying welcome#show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
   def show
+    @user = current_user
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   include UserHelpers
+  include SlackApiRequest
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,4 +16,19 @@ class User < ActiveRecord::Base
   def encrypted_phone_number=(val)
     write_attribute(:encrypted_phone_number, hash_data(val))
   end
+
+  def slack_team_name
+    slack_data["info"]["team"]
+  end
+
+  def slack_channels
+    channels = slack_get_channels(self)
+    channels.map { |channel| channel["name"] }
+  end
+
+  def slack_user_name
+    slack_data["info"]["user"]
+  end
+
+
 end
