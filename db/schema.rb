fbcbe1d4d35fc844de7696e3cc8618f2a9da5cc2
Zach H <zdhadgraft@gtcc.edu>
2016-07-01 14:11:22 -0400

    Switched to postgresql for dev/test setup.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701002811) do
+ActiveRecord::Schema.define(version: 20160701171534) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 20160701002811) do
     t.string   "slack_default_channel"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
